b2d47e402b2e3e266b116fa678a80bbc977fd880
DerekCresswell <derekjcress@gmail.com>
2019-07-29 15:14:44 -0700

    Removing and array manipulation added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 
 	let baseDate = new Date;
-
+	let list = [];
+	let compList = [];
+	
 	$(document).ready(function(){
-		
-		let list = [];
 
 		$("#addItem").click(function(){
 			
@@ -14,13 +14,25 @@
 			let item = {
 				value: newItem.val(),
 				date: new Date,
-				//
+				completed: undefined
 			}
 			
 			list.push(item);
 			newItem.val("");
 			
-			$("ol").append("<li>" + list[list.length - 1].value + "</li>");
+			$("ol").append("<li>" + list[list.length - 1].value + "<div class='deleteBTN'>X</div></li>");
+			//Picture may be prefered instead of X
+			
+		});
+		
+		$("#list").on("click", ".deleteBTN", function(){
+			
+			let toRem = $(this).closest("li");	
+			
+			compList.push(list.splice(toRem.index(), 1));
+			compList[compList.length - 1].completed = new Date;
+			
+			toRem.remove();
 			
 		});
 		
