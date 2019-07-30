473eefc286fe7488fb9eaf0e4e7618aa998af72b
DerekCresswell <derekjcress@gmail.com>
2019-07-30 14:59:12 -0700

    Improved Appends and other functions
    A few comments added describing problems and future plans.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-	let baseDate = new Date;
+	let baseDate = new Date();
 	let list = [];
 	let compList = [];
 	
@@ -8,41 +8,52 @@
 		$("#addItem").click(function(){
 			
 			let newItem = $("#inputText");
-			if(newItem.val() == "")
+			if(newItem.val() === "")
 				return;
 			
 			let item = {
 				value: newItem.val(),
-				date: new Date,
-				completed: undefined
+				date: new Date(),
+				completed: false
 			}
 			
 			list.push(item);
 			newItem.val("");
 			
-			$("ol#list").append("<li>" + list[list.length - 1].value + "<div class='deleteBTN'>X</div></li>");
-			//Picture may be prefered instead of X
+			$("ol#list").append($("<li>", {
+				html: item.value + " <div class='deleteBTN'>X</div>",
+				'data-index': list.length - 1
+			}));
 			
 		});
 		
 		$("#list").on("click", ".deleteBTN", function(){
 			
-			let toRem = $(this).closest("li");	
+			let toRem = $(this).closest("li");
+			let index = toRem.data('index');
 			
-			compList.push(list.splice(toRem.index(), 1)[0]);
-			compList[compList.length - 1].completed = new Date;
+			//If elm before this is removed "data-index" does not work \/ needs fixing
+			list[index].completed = new Date();
+			compList.push(list[index]);
+
+			let newIndex = compList.length - 1;
+			list.splice(index, 1);
 			
 			toRem.fadeOut(500, function(){
 				
 				toRem.remove();
-				let toAdd = $("ol#compList");
-				toAdd.append("<li id='compItem'>" + compList[compList.length - 1].value + "<ol class='compData'><li class='compDataItem'>temp</li></ol></li>");
-				toAdd.hide().fadeIn(500);
+				
+				$("ol#compList").append($("<li>", {
+					id: 'compItem',
+					html: compList[newIndex].value + "<ol class='compData'><li class='compDataItem'>TEMP</li></ol>", //Need data added
+					'data-index': newIndex
+				}));
+				
+				$("ol#compList").hide().fadeIn(500);
 				
 			});
 			
 		});
-		
 		
 		$("#list").on({mouseenter: function(){
 			
